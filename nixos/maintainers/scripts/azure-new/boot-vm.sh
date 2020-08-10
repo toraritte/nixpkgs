90b7c398301e5f1aa2f3428af2c7133229efdb4e
Attila Gulyas <toraritte@gmail.com>
2020-08-10 04:37:25 +0000

    save

--- nixos/maintainers/scripts/azure-new/boot-vm.sh
+++ nixos/maintainers/scripts/azure-new/boot-vm.sh
@@ -1,36 +1,140 @@
 #!/usr/bin/env bash
-set -euo pipefail
-set -x
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euxo pipefail
 
-image="${1}"
-location="westus2"
-group="nixos-test-vm"
-vm_size="Standard_D2s_v3";  os_size=42;
+usage() {
+  printf "-g --resource-group REQUIRED Created if does  not exist. Will"
+  printf "                             house a new disk and the created"
+  printf "                             image."
+  printf ""
+  printf "-i --image-id       REQUIRED Nix  expression   to  build  the"
+  printf "                             image. Defaults to"
+  printf "                             \"./examples/basic/image.nix\"."
+  printf ""
+  printf "-n --vm-name        REQUIRED The name of the  virtual machine"
+  printf "                             created."
+  printf ""
+  printf "-n --vm-size        See https://azure.microsoft.com/pricing/details/virtual-machines/ for size info."
+  printf "                    Default value: \"Standard_DS1_v2\""
+  printf ""
+  printf "-d --os-size        OS disk size in GB to create."
+  printf "                    Default value: \"42\""
+  printf ""
+  printf "-l --location       Values from `az account list-locations`."
+  printf "                    Default value: \"westus2\"."
+};
+ 
+# https://unix.stackexchange.com/a/204927/85131
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -l|--location)
+      location="$2"
+      ;;
+    -g|--resource-group)
+      group="$2"
+      ;;
+    -i|--image-id)
+      img_id="$2"
+      ;;
+    -n|--vm-name)
+      vm_name="$2"
+      ;;
+    -s|--vm-size)
+      vm_size="$2"
+      ;;
+    -d|--os-disk-size-gb)
+      os_size="$2"
+      ;;
+    *)
+      printf "***************************\n"
+      printf "* Error: Invalid argument *\n"
+      printf "***************************\n"
+      usage
+      exit 1
+  esac
+  shift
+  shift
+done
 
-# ensure group
-az group create --location "westus2" --name "${group}"
-group_id="$(az group show --name "${group}" -o tsv --query "[id]")"
-
-# (optional) identity
-if ! az identity show -n "${group}-identity" -g "${group}" &>/dev/stderr; then
-  az identity create --name "${group}-identity" --resource-group "${group}"
+if [ -z "${img_id}" ] || [ -z "${group}" ] || [ -z "${vm_name}" ];
+then
+  printf "************************************\n"
+  printf "* Error: Missing required argument *\n"
+  printf "************************************\n"
+  usage
+  exit 1
 fi
 
-# (optional) role assignment, to the resource group, bad but not really great alternatives
-identity_id="$(az identity show --name "${group}-identity" --resource-group "${group}" -o tsv --query "[id]")"
-principal_id="$(az identity show --name "${group}-identity" --resource-group "${group}" -o tsv --query "[principalId]")"
-until az role assignment create --assignee "${principal_id}" --role "Owner" --scope "${group_id}"; do sleep 1; done
+# ensure group
+if ! az group show --resource-group "${group}" &>/dev/null;
+then
+  az group create     \
+    --name "${group}" \
+    --location "${location:-"westus2"}"
+fi
+
+# (optional) identity
+if ! az identity show --name "${group}-identity" --resource-group "${group}" &>/dev/stderr;
+then
+  az identity create           \
+    --name "${group}-identity" \
+    --resource-group "${group}"
+fi
+
+# (optional) role assignment, to the resource group;
+# bad but not really great alternatives
+principal_id="$(
+  az identity show              \
+    --name "${group}-identity"  \
+    --resource-group "${group}" \
+    --output tsv --query "[principalId]"
+)"
+
+group_id="$(
+  az group show       \
+    --name "${group}" \
+    --output tsv      \
+    --query "[id]"
+)"
+
+assign_role() {
+  az role assignment create      \
+    --assignee "${principal_id}" \
+    --role "Owner"               \
+    --scope "${group_id}"
+}
+# As long as the `az role assignment` command fails,
+# the loop will continue
+# https://tldp.org/LDP/Bash-Beginners-Guide/html/sect_09_03.html
+# https://linuxize.com/post/bash-until-loop/
+# TODO I think this is superfluous
+until assign_role;
+do
+  echo "Retrying role assignment..."
+  sleep 1
+done
+
+echo "Role assignment successful"
+
+identity_id="$(
+  az identity show              \
+    --name "${group}-identity"  \
+    --resource-group "${group}" \
+    --output tsv --query "[id]"
+  )"
 
 # boot vm
 az vm create \
-  --name "${group}-vm" \
-  --resource-group "${group}" \
-  --assign-identity "${identity_id}" \
-  --size "${vm_size}" \
-  --os-disk-size-gb "${os_size}" \
-  --image "${image}" \
-  --admin-username "${USER}" \
-  --location "westus2" \
-  --storage-sku "Premium_LRS" \
-  --ssh-key-values "$(ssh-add -L)"
+  --name "${vm_name}"                   \
+  --resource-group "${group}"           \
+  --assign-identity "${identity_id}"    \
+  --size "${vm_size}"                   \
+  --os-disk-size-gb "${os_size}"        \
+  --image "${img_id}"                   \
+  --admin-username "${USER}"            \
+  --location "${location:-"westus2"}"   \
+  --storage-sku "Premium_LRS"           \
+  --generate-ssh-keys
+  # This only works if `ssh-agent` is running
+  # --ssh-key-values "$(ssh-add -L)"
 
--- nixos/maintainers/scripts/azure-new/upload-image.sh
+++ nixos/maintainers/scripts/azure-new/upload-image.sh
@@ -1,58 +1,138 @@
 #!/usr/bin/env bash
-set -euo pipefail
-set -x
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euxo pipefail
 
-image_nix="${1:-"./examples/basic/image.nix"}"
+show_id() {
+  az $1 show \
+    --resource-group "${group}" \
+    --name "${img_name}"        \
+    --output tsv --query "[id]" 
+}
 
-nix-build "${image_nix}" --out-link "azure"
+usage() {
+  echo '-g --resource-group REQUIRED Created if does  not exist. Will'
+  echo '                             house a new disk and the created'
+  echo '                             image.'
+  echo ''
+  echo '-n --image-name     REQUIRED The  name of  the image  created'
+  echo '                             (and also of the new disk).'
+  echo ''
+  echo '-i --image-nix      Nix  expression   to  build  the'
+  echo '                    image. Default value:'
+  echo '                    "./examples/basic/image.nix".'
+  echo ''
+  echo '-l --location       Values from `az account list-locations`.'
+  echo '                    Default value: "westus2".'
+}
+ 
+# https://unix.stackexchange.com/a/204927/85131
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -i|--image-nix)
+      image_nix="$2"
+      ;;
+    -l|--location)
+      location="$2"
+      ;;
+    -g|--resource-group)
+      group="$2"
+      ;;
+    -n|--image-name)
+      img_name="$2"
+      ;;
+    -h|--help)
+      usage
+      exit 1
+      ;;
+    *)
+      printf "***************************\n"
+      printf "* Error: Invalid argument *\n"
+      printf "***************************\n"
+      usage
+      exit 1
+  esac
+  shift
+  shift
+done
 
-group="nixos-images"
-location="westus2"
-img_name="nixos-image"
-img_file="$(readlink -f ./azure/disk.vhd)"
+if [ -z "${img_name}" ] || [ -z "${group}" ];
+then
+  printf "************************************\n"
+  printf "* Error: Missing required argument *\n"
+  printf "************************************\n"
+  usage
+  exit 1
+fi
 
-if ! az group show -n "${group}" &>/dev/null; then
-  az group create --name "${group}" --location "${location}"
+# Making  sure  that  one   is  logged  in  (to  avoid
+# surprises during the build).
+if [ $(az account list 2> /dev/null) = [] ];
+then
+  az login
+fi
+
+nix-build                                 \
+  --out-link "azure"                      \
+  "${image_nix:-"./examples/basic/image.nix"}"
+# nix-build "${image_nix}" --out-link "azure"
+
+if ! az group show --resource-group "${group}" &>/dev/null;
+then
+  az group create     \
+    --name "${group}" \
+    --location "${location:-"westus2"}"
 fi
 
 # note: the disk access token song/dance is tedious
 # but allows us to upload direct to a disk image
 # thereby avoid storage accounts (and naming them) entirely!
-if ! az disk show -g "${group}" -n "${img_name}" &>/dev/null; then
+
+if ! show_id "disk" &>/dev/null;
+then
+
+  img_file="$(readlink -f ./azure/disk.vhd)"
   bytes="$(stat -c %s ${img_file})"
-  size="30"
-  az disk create \
+
+  az disk create                \
     --resource-group "${group}" \
-    --name "${img_name}" \
-    --for-upload true --upload-size-bytes "${bytes}"
+    --name "${img_name}"        \
+    --for-upload true           \
+    --upload-size-bytes "${bytes}"
 
   timeout=$(( 60 * 60 )) # disk access token timeout
   sasurl="$(\
-    az disk grant-access \
-      --access-level Write \
-      --resource-group "${group}" \
-      --name "${img_name}" \
+    az disk grant-access               \
+      --access-level Write             \
+      --resource-group "${group}"      \
+      --name "${img_name}"             \
       --duration-in-seconds ${timeout} \
-        | jq -r '.accessSas'
+      --output tsv                     \
+      --query "[accessSas]"
   )"
+  # the --query part is not necessary (one column output)
 
   azcopy copy "${img_file}" "${sasurl}" \
-    --blob-type PageBlob
-
-  az disk revoke-access \
+    --blob-type PageBlob 
+    
+  # See public hypothes.is comment at
+  # https://docs.microsoft.com/en-us/cli/azure/disk?view=azure-cli-latest#az-disk-revoke-access
+  # but the gist is that
+  # > Revoking the SAS will  change the state of
+  # > the managed  disk and allow you  to attach
+  # > the disk to a VM.
+  az disk revoke-access         \
     --resource-group "${group}" \
     --name "${img_name}"
 fi
 
-if ! az image show -g "${group}" -n "${img_name}" &>/dev/null; then
-  diskid="$(az disk show -g "${group}" -n "${img_name}" -o json | jq -r .id)"
+if ! show_id "image" &>/dev/null;
+then
 
-  az image create \
-    --resource-group "${group}" \
-    --name "${img_name}" \
-    --source "${diskid}" \
+  az image create                \
+    --resource-group "${group}"  \
+    --name "${img_name}"         \
+    --source "$(show_id "disk")" \
     --os-type "linux" >/dev/null
 fi
 
-imageid="$(az image show -g "${group}" -n "${img_name}" -o json | jq -r .id)"
-echo "${imageid}"
+echo "$(show_id "image")"
